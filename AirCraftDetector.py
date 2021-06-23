766c485b6fe947be5bb3ce9c7b9946148720a8ba
MarcosPBatista <marcos.batista@usp.br>
2021-06-23 20:13:41 -0300

    Visualization Updated in order to see raw image and ORB Feature Detection in place

--- AirCraftDetector.py
+++ AirCraftDetector.py
@@ -6,31 +6,13 @@ Created on Fri Jun  4 11:04:28 2021
 """
 
 import cv2 as cv
-#import numpy as np
+import numpy as np
+from msvcrt import getch
 #import matplotlib.pyplot as plt
 
 cap = cv.VideoCapture(r"C:\Users\marco\Desktop\Profissional\USP\Mestrado\ProcessamentoImagem\FinalProject\AirCraft-Detection\Dataset\EVision_VideoDataset\1FNB737.avi")
 #Initialize ORB
 orb = cv.ORB_create()
-
-#Initialize Optical Flow Parameters
-# params for ShiTomasi corner detection
-feature_params = dict( maxCorners = 100,
-                       qualityLevel = 0.3,
-                       minDistance = 7,
-                       blockSize = 7 )
-# Parameters for lucas kanade optical flow
-lk_params = dict( winSize  = (15,15),
-                  maxLevel = 2,
-                  criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
-
-# Create some random colors
-color = np.random.randint(0,255,(100,3))
-
-# Take first frame and find corners in it
-ret, old_frame = cap.read()
-old_gray = cv.cvtColor(old_frame, cv.COLOR_BGR2GRAY)
-p0 = cv.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
 
 
 while(cap.isOpened()):
@@ -39,21 +21,26 @@ while(cap.isOpened()):
     
     if ret == True:
         #Test ORB in this images.
-# =============================================================================
-#         #Detect features with ORB
-#         kp = orb.detect(frame,None)
-#         kp, des = orb.compute(frame, kp)
-#         
-#         #Plot the Keypoints in the Original Image. Use Green color
-#         img_final = cv.drawKeypoints(frame, kp, None, color=(0,255,0), flags=0)
-# =============================================================================
+        #Detect features with ORB
+        kp = orb.detect(frame,None)
+        kp, des = orb.compute(frame, kp)
         
+        #Plot the Keypoints in the Original Image. Use Green color
+        img_final = cv.drawKeypoints(frame, kp, None, color=(0,255,0), flags=0)
         
-        cv.imshow('Frame',img_final)
+        reduced_frame = cv.resize(frame,(0,0), None, 0.5, 0.5)
+        reduced_img_final = cv.resize(img_final,(0,0), None, 0.5, 0.5)
+                
+        img_stack = np.hstack((reduced_frame, reduced_img_final))
         
-        #Use 'q' in Keyboard to stop the video.
-        if cv.waitKey(50) & 0xFF == ord('q'):
+        cv.imshow('Stacked Image', img_stack)
+        
+        #Use 'q' in Keyboard to stop the video or Space (' ') to run step by step.
+        Keyboard = cv.waitKey(1) & 0xFF
+        if Keyboard == ord('q'):
             break
+        elif Keyboard == ord(' '):
+            cv.waitKey(0)
+            continue
         
-cv.waitKey(0)
 cv.destroyAllWindows()
