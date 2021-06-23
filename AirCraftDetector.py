246c56e17522a2b4f47f98e745a05d9031724c89
MarcosPBatista <marcos.batista@usp.br>
2021-06-22 21:11:27 -0300

    Lucas-Kanade Test

--- AirCraftDetector.py
+++ AirCraftDetector.py
@@ -6,10 +6,10 @@ Created on Fri Jun  4 11:04:28 2021
 """
 
 import cv2 as cv
-#import numpy as np
+import numpy as np
 #import matplotlib.pyplot as plt
 
-cap = cv.VideoCapture(r"C:\Users\marco\Desktop\Profissional\USP\Mestrado\ProcessamentoImagem\FinalProject\AirCraft-Detection\Dataset\EVision_VideoDataset\1FNB737.avi")
+cap = cv.VideoCapture(r"C:\Users\marco\Desktop\Profissional\USP\Mestrado\ProcessamentoImagem\FinalProject\AirCraft-Detection\Dataset\EVision_VideoDataset\slow_traffic_small.mp4")
 #Initialize ORB
 orb = cv.ORB_create()
 
@@ -24,6 +24,8 @@ lk_params = dict( winSize  = (15,15),
                   maxLevel = 2,
                   criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
 
+
+
 # Create some random colors
 color = np.random.randint(0,255,(100,3))
 
@@ -31,7 +33,8 @@ color = np.random.randint(0,255,(100,3))
 ret, old_frame = cap.read()
 old_gray = cv.cvtColor(old_frame, cv.COLOR_BGR2GRAY)
 p0 = cv.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
-
+# Create a mask image for drawing purposes
+mask = np.zeros_like(old_frame)
 
 while(cap.isOpened()):
     #Read image
@@ -47,13 +50,37 @@ while(cap.isOpened()):
 #         #Plot the Keypoints in the Original Image. Use Green color
 #         img_final = cv.drawKeypoints(frame, kp, None, color=(0,255,0), flags=0)
 # =============================================================================
+        frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        
+        # calculate optical flow
+        p1, st, err = cv.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+        
+        # Select good points
+        if p1 is not None:
+            good_new = p1[st==1]
+            good_old = p0[st==1]     
+        
+        # draw the tracks
+        for i,(new,old) in enumerate(zip(good_new, good_old)):
+            a,b = new.ravel()
+            c,d = old.ravel()
+            mask = cv.line(mask, (int(a),int(b)),(int(c),int(d)), color[i].tolist(), 2)
+            frame = cv.circle(frame,(int(a),int(b)),5,color[i].tolist(),-1)
         
         
-        cv.imshow('Frame',img_final)
+        img = cv.add(frame,mask)
+ 
+        
+        cv.imshow('Frame', img)
+        
+        # Now update the previous frame and previous points
+        old_gray = frame_gray.copy()
+        p0 = good_new.reshape(-1,1,2)
         
         #Use 'q' in Keyboard to stop the video.
         if cv.waitKey(50) & 0xFF == ord('q'):
             break
+
         
 cv.waitKey(0)
 cv.destroyAllWindows()
